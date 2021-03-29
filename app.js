cd62cd2d6f9dd0f809bf300ffae6f20b8ddc8150
prayojitha <prayojitha.bere@gmail.com>
2021-03-28 23:05:14 -0500

    working with query strings

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var electronicsRouter = require('./routes/electronics');
+var starsRouter = require('./routes/stars');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/index', indexRouter);
 app.use('/users', usersRouter);
 app.use('/electronics', electronicsRouter);
+app.use('/stars', starsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
